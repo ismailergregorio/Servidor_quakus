fde0548e0881880c6b81567a262f99ceee73ac79
ismailergregorio <64173567+ismailergregorio@users.noreply.github.com>
2025-06-05 17:53:32 -0300

    Juste api separação

---
+++ api_entregas/src/main/java/com/example/controllers/ControleDeEntregas.java
@@ -0,0 +1,50 @@
+package com.example.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import com.example.models.Entregas;
+import com.example.repository.RepositorioEntregas;
+
+import jakarta.ws.rs.core.MediaType;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
+
+@Path("/api/entregas")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Tag(name = "Entregas", description = "Operaçoes para gerenciamento de Entregas")
+
+public class ControleDeEntregas {
+   @Inject
+   RepositorioEntregas repositorioentregas;
+   // private List<modeloEntregas> entregas = new ArrayList<>();
+   @GET
+   public List<Entregas> ListaEntregas() {
+      return repositorioentregas.listAll();
+   }
+
+   @POST
+   @Transactional
+   public Response criarEntregas(Entregas entrega) {
+      try{
+         repositorioentregas.persist(entrega);
+         return Response.status(201).build();
+      } catch(Exception e){
+         return Response.serverError().entity(e).build();
+      }
+   }
+
+   public boolean agendarEntregas() {
+      return true;
+   }
+
+}
---
+++ api_entregas/src/main/java/com/example/models/Entregas.java
@@ -0,0 +1,54 @@
+package com.example.models;
+import java.sql.Date;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Entregas")
+
+public class Entregas {
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private long id;
+ private String categoria;
+ @Column(nullable = false)
+ private Date dataDeSaidaDeEntrega;
+ private int volume;
+
+ public int getVolume() {
+  return volume;
+ }
+
+ public void setVolume(int volume) {
+  this.volume = volume;
+ }
+
+ public long getId() {
+  return id;
+ }
+
+ public void setId(long id) {
+  this.id = id;
+ }
+
+ public String getCategoria() {
+  return categoria;
+ }
+
+ public void setCategoria(String categoria) {
+  this.categoria = categoria;
+ }
+
+ public Date getDataDeSaidaDeEntrega() {
+  return dataDeSaidaDeEntrega;
+ }
+
+ public void setDataDeSaidaDeEntrega(Date dataDeSaidaDeEntrega) {
+  this.dataDeSaidaDeEntrega = dataDeSaidaDeEntrega;
+ }
+
+}
